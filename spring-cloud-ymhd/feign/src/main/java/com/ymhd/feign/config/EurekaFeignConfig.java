3b26812cf0a523b370d23a4bf171128de5c0d657
hly <18957143241@163.com>
2019-05-29 23:43:56 +0800

    增加eureka-hystrix-dashboard断路器指标看板模块（同时在ribbon和feign里添加注册断路器指标流Servlet）

---
+++ spring-cloud-ymhd/feign/src/main/java/com/ymhd/feign/config/EurekaFeignConfig.java
@@ -0,0 +1,25 @@
+package com.ymhd.feign.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * EurekaFeignConfig
+ * 应用配置类，初始化 Bean和配置信息
+ */
+@Configuration
+public class EurekaFeignConfig {
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
+}
--- spring-cloud-ymhd/ribbon/src/main/java/com/ymhd/ribbon/config/EurekaRibbonConfig.java
+++ spring-cloud-ymhd/ribbon/src/main/java/com/ymhd/ribbon/config/EurekaRibbonConfig.java
@@ -1,5 +1,7 @@
 package com.ymhd.ribbon.config;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,5 +20,14 @@ public class EurekaRibbonConfig {
     @LoadBalanced // 实现负载均衡
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
